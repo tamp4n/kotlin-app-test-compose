2323874b2bca6df162c80d081472ae19b3574470
tamp4n <tampan@gmail.com>
2025-06-10 23:54:56 +0700

    Tes menampilkan list model pokemon di log MainActivity.kt

--- app/src/main/java/com/example/apptestcompose/MainActivity.kt
+++ app/src/main/java/com/example/apptestcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.apptestcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.apptestcompose.model.getData
 import com.example.apptestcompose.ui.theme.AppTestComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        getData().forEach {
+            Log.d("MainActivity", it.name)
+        }
+
+        Log.d("MainActivity", "contoh Log")
+
         setContent {
             AppTestComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
