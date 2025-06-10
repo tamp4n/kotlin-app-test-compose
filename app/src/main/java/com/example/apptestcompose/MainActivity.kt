ff50e0177bbe0178924acf00805c892e8cf4e066
tamp4n <tampan@gmail.com>
2025-06-10 23:15:20 +0700

    coba edit di MainActivity.kt

--- app/src/main/java/com/example/apptestcompose/MainActivity.kt
+++ app/src/main/java/com/example/apptestcompose/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppTestComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Joyo Kusumo Nugraha",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
