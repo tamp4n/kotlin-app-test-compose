125550799f72bac9af729b954fef56fa1635a7d2
tamp4n <tampan@gmail.com>
2025-06-11 08:58:56 +0700

    edit file PokemonActivity.kt (PokemonImage)

--- app/src/main/java/com/example/apptestcompose/pages/PokemonActivity.kt
+++ app/src/main/java/com/example/apptestcompose/pages/PokemonActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.content.MediaType.Companion.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.apptestcompose.pages.ui.theme.AppTestComposeTheme
+import com.example.apptestcompose.R
 
 class PokemonActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +30,10 @@ class PokemonActivity : ComponentActivity() {
         setContent {
             AppTestComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
+                    PokemonImage(
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
@@ -31,17 +41,14 @@ class PokemonActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PokemonImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.pikachu),
+        contentDescription = "Pikachu",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(84.dp)
+            .padding(8.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    AppTestComposeTheme {
-        Greeting2("Android")
-    }
 }
