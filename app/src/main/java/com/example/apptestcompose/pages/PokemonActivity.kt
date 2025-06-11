9ab8f1eb49cba6d6c4258db5524bf1a280333c29
tamp4n <tampan@gmail.com>
2025-06-11 12:50:08 +0700

    edit file PokemonActivity.kt (fun PokemonContent)

--- app/src/main/java/com/example/apptestcompose/pages/PokemonActivity.kt
+++ app/src/main/java/com/example/apptestcompose/pages/PokemonActivity.kt
@@ -6,22 +6,35 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.content.MediaType.Companion.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.apptestcompose.pages.ui.theme.AppTestComposeTheme
 import com.example.apptestcompose.R
+import com.example.apptestcompose.model.PokemonModel
+import com.example.apptestcompose.model.getData
 
 class PokemonActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +43,7 @@ class PokemonActivity : ComponentActivity() {
         setContent {
             AppTestComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PokemonImage(
+                    PokemonContent(
                         modifier = Modifier.padding(innerPadding)
                     )
 
@@ -41,10 +54,61 @@ class PokemonActivity : ComponentActivity() {
 }
 
 @Composable
-fun PokemonImage(modifier: Modifier = Modifier) {
+fun PokemonContent(modifier: Modifier = Modifier) {
+    val pokemons: List<PokemonModel> = getData()
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
+        modifier = modifier
+    ) {
+        items(
+            items = pokemons,
+            itemContent = { PokemonItem(it) }
+        )
+    }
+}
+
+
+@Composable
+fun PokemonItem(pokemon: PokemonModel) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.elevatedCardElevation(2.dp),
+        colors = CardDefaults.elevatedCardColors(contentColor = Color.White),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp))
+    ) {
+        Row {
+            PokemonImage(pokemon = pokemon)
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    text = pokemon.name,
+                    style = typography.headlineMedium,
+                    color = Color.Black
+                )
+                Text(
+                    text = "View Details",
+                    style = typography.bodyMedium,
+                    color = Color.Black
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun PokemonImage(pokemon: PokemonModel) {
     Image(
-        painter = painterResource(id = R.drawable.pikachu),
-        contentDescription = "Pikachu",
+//        painter = painterResource(id = R.drawable.pikachu),
+        painter = painterResource(id = pokemon.image),
+
+        contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = Modifier
             .size(84.dp)
